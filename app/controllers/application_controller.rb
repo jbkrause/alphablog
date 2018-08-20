c1db8f916c9a14605b887e5014f4ef8f6c2cdd08
Jan Krause <j.krause@docuteam.ch>
2018-08-20 09:33:45 +0200

    restricted links display according to login status (temp)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+
+  # makes methods available to views
+  helper_method :current_user, :logged_in?
+  
+  def current_user
+    # avoids hitting database if already set
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def requires_user
+    if !logged_in?
+      falsh[:danger] = "you must be logged in for that action"
+      redirect_to root_path
+    end
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:edit, :update, :show]
+
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
   end
@@ -19,11 +21,11 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id]) #see before_action
   end
 
   def update
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id]) #see before_action
     if @user.update(user_params)
       flash[:success] = "your acount was updated"
       redirect_to articles_path
@@ -33,7 +35,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id]) #see before_action
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
@@ -41,6 +43,10 @@ class UsersController < ApplicationController
   
     def user_params
       params.require(:user).permit(:username, :email, :password)
+    end
+
+    def set_user
+      @user = User.find(params[:id])
     end
     
 end
