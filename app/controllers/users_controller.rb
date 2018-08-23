e6e6cc43dd55ee7b8916e8384507a55e3173d0c7
Jan Krause <j.krause@docuteam.ch>
2018-08-23 16:20:11 +0200

    add sign in upon sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,8 +14,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "welcome to the appha blog #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else # if incorect user, we display new usertemplate, but with errors displaye by partial
       render 'new'
     end
